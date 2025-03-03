682afec8d613cb70c36418726168533ebc26cd86
Markus Heinonen <markus.heinonen@gmail.com>
2025-03-03 15:03:01 +0200

    Create download-datasets.sh

---
+++ data/download-datasets.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+wget https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz
+wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
+wget https://zenodo.org/records/2535967/files/CIFAR-10-C.tar
+wget https://zenodo.org/records/3555552/files/CIFAR-100-C.tar
+wget https://zenodo.org/records/2536630/files/Tiny-ImageNet-C.tar
